775068511b3112893c3c9cb01004a409489eb6f1
leetoryy <nayoung0736@naver.com>
2023-11-23 12:49:54 +0000

    [Feat] render_template기능 추가

--- app.py
+++ app.py
@@ -2,35 +2,35 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/join.html') 
+@app.route('/join') 
 def join_html():
     return render_template('counselor/join.html')
 
-@app.route('/counselor/home.html') 
+@app.route('/counselor/home') 
 def counselor_home_html():
     return render_template('counselor/home.html')
 
-@app.route('/counselor/child_list.html') 
+@app.route('/counselor/child_listl') 
 def child_list_html():
     return render_template('counselor/child_list.html')
 
-@app.route('/user/home.html') 
+@app.route('/user/home') 
 def user_home_html():
     return render_template('user/home.html')
 
-@app.route('/user/mbti_home.html') 
+@app.route('/user/mbti_homel') 
 def mbti_home_html():
     return render_template('user/mbti_home.html')
 
-@app.route('/user/mbti_match.html') 
+@app.route('/user/mbti_match') 
 def mbti_match_html():
     return render_template('user/mbti_match.html')
 
-@app.route('/user/mbti_result.html') 
+@app.route('/user/mbti_result') 
 def mbti_result_html():
     return render_template('user/mbti_result.html')
 
-@app.route('/user/mbti_test.html') 
+@app.route('/user/mbti_test') 
 def mbti_test_html():
     return render_template('user/mbti_test.html')
 
