09ae815ef66bf58d54028cdd0f2847c6f40a9502
hjsu0825 <hjsu0825@naver.com>
2023-12-16 18:05:08 +0900

    [Feat] 결과 화면 이미지 추가

--- static/javascript/user/mbti_result.js
+++ static/javascript/user/mbti_result.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
   const resultTitle = document.querySelector('.js-resultTitle');
   const resultExplain = document.querySelector('.js-resultExplain');
+  const mbtiImage = document.querySelector('.js-mbtiImage');
   const restartButton = document.querySelector('.js-restart');
   const matchButton = document.querySelector('.js-match');
 
@@ -15,51 +16,67 @@ document.addEventListener('DOMContentLoaded', function() {
     switch (result) {
       case 'ISTJ':
         resultExplain.textContent = '바른 생활 어린이';
+        mbtiImage.src = '/static/images/ISTJ.png';
         break;
       case 'ISFJ':
         resultExplain.textContent = '예의 바른 어린이';
+        mbtiImage.src = '/static/images/ISFJ.png';
         break;
       case 'INFJ':
         resultExplain.textContent = '우정 가득 어린이';
+        mbtiImage.src = '/static/images/INFJ.png';
         break;
       case 'INTJ':
         resultExplain.textContent = '성숙한 어린이';
+        mbtiImage.src = '/static/images/INTJ.png';
         break;
       case 'ISTP':
         resultExplain.textContent = '사교적인 어린이';
+        mbtiImage.src = '/static/images/ISTP.png';
         break;
       case 'ISFP':
         resultExplain.textContent = '여유로운 어린이';
+        mbtiImage.src = '/static/images/ISFP.png';
         break;
       case 'INFP':
         resultExplain.textContent = '인정 많은 어린이';
+        mbtiImage.src = '/static/images/INFP.png';
         break;
       case 'INTP':
         resultExplain.textContent = '척척박사 어린이';
+        mbtiImage.src = '/static/images/INTP.png';
         break;
       case 'ESTP':
         resultExplain.textContent = '참견쟁이 어린이';
+        mbtiImage.src = '/static/images/ESTP.png';
         break;
       case 'ESFP':
         resultExplain.textContent = '적극적인 어린이';
+        mbtiImage.src = '/static/images/ESFP.png';
         break;
       case 'ENFP':
         resultExplain.textContent = '변덕쟁이 어린이';
+        mbtiImage.src = '/static/images/ENFP.png';
         break;
       case 'ENTP':
         resultExplain.textContent = '개인주의 어린이';
+        mbtiImage.src = '/static/images/ENTP.png';
         break;
       case 'ESTJ':
         resultExplain.textContent = '모범적인 어린이';
+        mbtiImage.src = '/static/images/ESTJ.png';
         break;
       case 'ESFJ':
         resultExplain.textContent = '발표대장 어린이';
+        mbtiImage.src = '/static/images/ESFJ.png';
         break;
       case 'ENFJ':
         resultExplain.textContent = '사차원적 어린이';
+        mbtiImage.src = '/static/images/ENFJ.png';
         break;
       case 'ENTJ':
         resultExplain.textContent = '원칙주의 어린이';
+        mbtiImage.src = '/static/images/ENTJ.png';
         break;
     }
   }
